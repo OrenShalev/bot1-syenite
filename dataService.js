2349786d5976e6d20c4c10e02ac7b82f9f7c53fe
Oren Shalev <shalev.oren@gmail.com>
2018-04-14 12:13:26 +0300

    Rewrote dataService.

--- dataService.js
+++ dataService.js
@@ -1,10 +1,29 @@
 const fs = require(`fs`);
 
+const dataFile = `.data/data.json`;
+let data = {};
+
+module.exports = { load, save, isItemTweeted, markItemAsTweeted };
+
+function load() {
+    data = readJson(dataFile);
+}
+function save() {
+    writeJson(dataFile, data);
+}
+
+function isItemTweeted(itemLink) {
+    data.tweetedLinks = data.tweetedLinks || {};
+    return data.tweetedLinks[itemLink] === true;
+}
+function markItemAsTweeted(itemLink) {
+    data.tweetedLinks = data.tweetedLinks || {};
+    data.tweetedLinks[itemLink] = true;
+}
+
 function readJson(filename) {
     return JSON.parse(fs.readFileSync(filename));
 }
 function writeJson(filename, json) {
     fs.writeFileSync(filename, JSON.stringify(json, ' ', 4), { encoding: `utf8` });
 }
-
-module.exports = { readJson, writeJson };
