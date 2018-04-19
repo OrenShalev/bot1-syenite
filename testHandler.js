a3eefde422ec9b460d03c7667f2d534b682be769
Oren Shalev <shalev.oren@gmail.com>
2018-04-19 21:51:01 +0300

    Refactor handler to pick RSS feed and twitter handle based on :key request param.

--- testHandler.js
+++ testHandler.js
@@ -1,8 +1,12 @@
 const dataService = require(`./dataService`);
 const readRss = require(`./rssReader`);
-const {postTweet} = require(`./twitterService`);
+const {setupAndTweet} = require(`./twitterService`);
 
 const dataFilename = `.data/data.json`;
+
+const defaultRssFeed = `http://rss.walla.co.il/feed/6?type=main`; // Walla tech, no particular reason.
+const defaultTwitterAccount = `bot1_syenite`; // "It's alive!"
+
 module.exports = testHandler;
 
 async function testHandler(req, res) {
@@ -15,19 +19,22 @@ async function testHandler(req, res) {
         Save data
         */
 
+        const key = req.params.key;
+        const {rssUrl, twitterHandle} = resolveRssAndTwitterFromRequest(key);
+
         try {
             dataService.load();
         } catch (e) {
-            sendError(res, `Failed to load data.`, e);
+            sendError(res, `[${key}] Failed to load data.`, e);
             return;
         }
 
         let feed;
         try {
-            feed = await readRss(`http://rss.walla.co.il/feed/6?type=main`);
+            feed = await readRss(rssUrl);
             // const feed = await readRss(`http://localhost:3000/.mock-data/mock-feed.xml`);
         } catch (e) {
-            sendError(res, `Failed to load RSS feed`, e);
+            sendError(res, `[${key}] Failed to load RSS feed`, e);
             return;
         }
 
@@ -43,9 +50,9 @@ async function testHandler(req, res) {
             if (shouldTweet) { // then build a string from item
                 const status = createStatus(item);
                 try {
-                    postTweet(status);
+                    setupAndTweet({twitterHandle, status});
                 } catch (e) {
-                    sendError(res, `Failed to post tweet`, e);
+                    sendError(res, `[${key}] Failed to post tweet`, e);
                     return;
                 }
 
@@ -53,20 +60,47 @@ async function testHandler(req, res) {
                 try {
                     dataService.save();
                 } catch (e) {
-                    sendError(res, `Failed to save data, we may soon have a repeating tweet...`, e);
+                    sendError(res, `[${key}] Failed to save data, we may soon have a repeating tweet...`, e);
                     return;
                 }
 
-                statuses.push(`Tweeted ${item.title}`);
+                statuses.push(`[${key}] Tweeted ${item.title}`);
                 break candidates;
             } else {
-                statuses.push(`Not tweeted ${item.title}`);
+                statuses.push(`[${key}] Not tweeted ${item.title}`);
             }
         }
 
         res.status(200).send(statuses.join(`\n`));
     } catch (e) {
-        sendError(res, `General error.`, e);
+        sendError(res, `[${key}] General error.`, e);
+    }
+}
+
+const key2Details = new Map([
+    [`irrelevant`, {
+        rssUrl: `https://irrelevant.org.il/feed`,
+        twitterHandle: `irrelevant_il`
+    }],
+    [`dailytech`, {
+        rssUrl: `https://www.calcalist.co.il/GeneralRSS/0,16335,L-3862,00.xml`,
+        twitterHandle: `dailytech_il`
+    }],
+    [`syenite`, {
+        rssUrl: defaultRssFeed,
+        twitterHandle: defaultTwitterAccount
+    }],
+    [`default`, {
+        rssUrl: defaultRssFeed,
+        twitterHandle: defaultTwitterAccount
+    }]
+]);
+
+function resolveRssAndTwitterFromRequest(key) {
+    if (key2Details.has(key)) {
+        return key2Details.get(key);
+    } else {
+        return key2Details.get(`default`);
     }
 }
 
